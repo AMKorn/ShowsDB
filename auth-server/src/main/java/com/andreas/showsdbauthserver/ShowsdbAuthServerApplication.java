4eefd5673b5eb8a0555b60e050b147d1629aa22d
Andreas Korn <andreas.korn@plexus.es>
2024-04-24 08:53:46 +0200

    feat: create auth server

---
+++ auth-server/src/main/java/com/andreas/showsdbauthserver/ShowsdbAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.andreas.showsdbauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowsdbAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowsdbAuthServerApplication.class, args);
+	}
+
+}
---
+++ auth-server/src/test/java/com/andreas/showsdbauthserver/ShowsdbAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreas.showsdbauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowsdbAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
