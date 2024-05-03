2bda6f4c7cfbef127f80ae4b1055e1cc9fa33fc0
Andreas Korn <andreas.korn@plexus.es>
2024-05-03 08:48:41 +0200

    feat: fully dockerize database

--- client-server/db/changelog-auth.sql
+++ client-server/db/changelog-auth.sql
@@ -27,3 +27,11 @@ create table `user_role`(
 --changeset andreas:16 labels:users
 insert into `role`(`role`) values ('ADMIN'), ('USER')
 --rollback TRUNCATE `role`;
+
+--changeset andreas:17 labels:users
+insert into `user`(`username`,`password`) values ('admin', '$2a$10$xSQPn9fPDmGB88ApFtjfu.8GbNt5NOoufyW3UN8p9yGJ4It0b/EiK'), ('user', '$2a$10$xSQPn9fPDmGB88ApFtjfu.8GbNt5NOoufyW3UN8p9yGJ4It0b/EiK');
+--rollback TRUNCATE `user`;
+
+--changeset andreas:18 labels:users
+insert into `user_role` values (1, 1), (1, 2), (2, 2);
+--rollback TRUNCATE `user_role`;
