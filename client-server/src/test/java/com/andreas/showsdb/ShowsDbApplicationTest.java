0547be0d6500215308d0ff116fd21d19c08922ba
Andreas Korn <andreas.korn@plexus.es>
2024-05-16 11:38:54 +0200

    tests: add assertion to contextLoads

--- client-server/src/test/java/com/andreas/showsdb/ShowsDbApplicationTest.java
+++ client-server/src/test/java/com/andreas/showsdb/ShowsDbApplicationTest.java
@@ -1,12 +1,20 @@
 package com.andreas.showsdb;
 
+import com.andreas.showsdb.controller.ShowsController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.WHEN_AVAILABLE)
 class ShowsDbApplicationTest {
-    @Test
-    void testMain() {
 
+    @Autowired
+    private ShowsController showsController;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(showsController);
     }
 }
