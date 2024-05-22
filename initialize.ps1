4d13f374b42ecc86cbb0a238631a5dc187a68792
Andreas Korn <andreas.korn@plexus.es>
2024-05-22 11:31:10 +0200

    feat: improve initialize script

--- initialize.ps1
+++ initialize.ps1
@@ -5,7 +5,14 @@ param(
     [switch]$i
 )
 if($i){
-    Write-Output "** Getting private IP **"
+    Write-Output "** Updating private IP **"
+    
+    # Write-Output 'Previous .env:'
+    $fileExisted = Test-Path '.env'
+    if($fileExisted){
+        $old = Get-Content .env
+    }
+
     # Get the local computer's IPv4 address
     $env:HostIP = (
         Get-NetIPConfiguration |
@@ -17,6 +24,13 @@ if($i){
 
     # Write the IP address to a .env file
     "privateip=$env:HostIP" | Out-File -FilePath '.env' -Encoding UTF8 -Force
+
+    # Write-Output 'New .env:'
+    $new = Get-Content .env
+    $wasIpUpdated = -not $fileExisted -or $old -ne $new
+    if($wasIpUpdated) {
+        Write-Output 'IP changed'
+    }
 }
 if($a){
     Write-Output "** Compiling auth server **"
@@ -39,4 +53,8 @@ if($d){
     Set-Location client-server
     mvn liquibase:update
     Set-Location ..
+}
+Write-Output "** Process finished **"
+if($wasIpUpdated) {
+    Write-Output "Remember to update the IP on any places you may have manually written it (like PostMan) to its new value: $env:HostIP"
 }
